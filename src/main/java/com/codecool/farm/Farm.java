37a57fea2467f79be3eef32edff8fc29bdf0c074
DurayBence <duray.bence@users.noreply.github.com>
2021-03-24 15:24:07 +0100

    Finish task
    Implement Farm

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,47 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
 
+    List<Animal> animals = new ArrayList<>();
+
+    public Farm(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void feedAnimals() {
+        for (Animal a : animals) {
+            a.feed();
+        }
+    }
+
+    public void butcher(Butcher butcher) {
+        for (int i = 0; i < animals.size(); i++) {
+            if (butcher.canButcher(animals.get(i))) {
+                animals.remove(i);
+                i--;
+            }
+        }
+    }
+
+    public boolean isEmpty() {
+        return animals.isEmpty();
+    }
+
+    public List<String> getStatus() {
+        List<String> result = new ArrayList<>();
+        for (Animal a : animals) {
+            result.add("There is a " + a.getSize() + " sized " +
+                                       a.getClass().getSimpleName().toLowerCase() + " in the farm.");
+        }
+        return result;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
 }
