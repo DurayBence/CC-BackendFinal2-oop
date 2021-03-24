618e6b303bab3e1fa217af82f3cccad6bfb772ec
DurayBence <duray.bence@users.noreply.github.com>
2021-03-24 15:13:18 +0100

    Implement animals

--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -2,4 +2,13 @@ package com.codecool.farm.animal;
 
 public abstract class Animal {
 
+    int size = 0;
+
+    public Animal() { }
+
+    public abstract void feed();
+
+    public int getSize() {
+        return size;
+    }
 }
